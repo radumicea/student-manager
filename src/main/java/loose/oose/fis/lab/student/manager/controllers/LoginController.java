e94c7f8377df362e1e2233d91662c7e0dfad630e
Radu Micea <radu.micea@avaelgo.ro>
2022-03-29 15:08:57 +0300

    Fixed vscode

--- src/main/java/loose/oose/fis/lab/student/manager/controllers/LoginController.java
+++ src/main/java/loose/oose/fis/lab/student/manager/controllers/LoginController.java
@@ -1,9 +1,15 @@
 package loose.oose.fis.lab.student.manager.controllers;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class LoginController {
 
